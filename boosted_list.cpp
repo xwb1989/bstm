c191b98964a611911b04cd34f8bb53de3606477c
Wenbin Xiao <xwb1989@gmail.com>
2014-10-15 23:39:55 -0400

    sketch

---
+++ boosted_list.cpp
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  boosted_list.cpp
+ *
+ *    Description:  definition of BoostedList
+ *
+ *        Version:  1.0
+ *        Created:  10/15/2014 07:44:05 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wenbin Xiao (github.com/xwb1989)
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "boosted_list.h"
+
+
+
+
