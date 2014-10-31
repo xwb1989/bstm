54b693016a7507b0bf46430300dfd2ffc0ed84f4
Wenbin Xiao <xwb1989@gmail.com>
2014-10-30 21:11:18 -0400

    basic version

--- lib_test/wl_runner.py
+++ lib_test/wl_runner.py
@@ -2,30 +2,69 @@
 
 import os
 import subprocess
+import datetime
 
 CLEAN = "make clean"
 MAKE_TL2 = "make tl2"
 MAKE_BOOST = "make boost"
 
 CMD = "./work_load "
-HIGH_CONTENTION_DISJOINT = "-c4 -n8 -r128 -p25 -t65536"
-HIGH_CONTENTION_OVERLAPPED = "-c4 -n8 -r128 -p50 -t65536"
 
+max_clients = 16
+min_clients = 1
+
+max_sets = 16
+min_sets = 1
+
+max_size = 128
+min_size = 8
+
+max_percent = 100
+min_percent = 25
+percent_step = 25
+
+num_tx = 65536
+
+reps = 10
+
+def run_cmd(output):
+    print("Creating file: " + output_file) 
+    subprocess.call("touch " + output_file, shell=True)
+    curr_client = min_clients
+    while curr_client <= max_clients:
+        curr_sets = min_sets
+        while curr_sets <= max_sets:
+            curr_size = min_size
+            while curr_size <= max_size:
+                curr_percent = min_percent
+                while curr_percent <= max_percent:
+                    args = " -c%i -n%i -r%i -p%i -t%i" %(curr_client, curr_sets, curr_size, curr_percent, num_tx)
+                    cmd = CMD + args + " >> " + output
+                    print("Runing with cmd: " + cmd) 
+                    for i in range(reps):
+                        subprocess.call(cmd, shell=True)
+                    curr_percent += percent_step
+                curr_size *= 2
+            curr_sets *= 2
+        curr_client *= 2
 
 
 
 if __name__ == "__main__":
-    subprocess.call(CLEAN, shell=True)
+
+    if not os.path.isdir("output"):
+        os.makedirs("output")
+    today = datetime.datetime.today().strftime("%Y%m%d%H%M%S")
 
     print("Compiling TL2")
+    subprocess.call(CLEAN, shell=True)
     subprocess.call(MAKE_TL2, shell=True)
+    output_file = "output/" + today + "-tl2"
+    run_cmd(output_file)
 
-    subprocess.call(CMD + HIGH_CONTENTION_OVERLAPPED, shell=True)
-    subprocess.call(CMD + HIGH_CONTENTION_DISJOINT, shell=True)
     
-
+    print("Compiling boost")
     subprocess.call(CLEAN, shell=True)
     subprocess.call(MAKE_BOOST, shell=True)
-
-    subprocess.call(CMD + HIGH_CONTENTION_OVERLAPPED, shell=True)
-    subprocess.call(CMD + HIGH_CONTENTION_DISJOINT, shell=True)
+    output_file = "output/" + today + "-boost"
+    run_cmd(output_file)
