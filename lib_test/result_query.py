5cb888a6b3506f8f506048db580196d1f2d3a9cf
Wenbin Xiao <xwb1989@gmail.com>
2014-11-04 12:16:40 -0500

    version 0.0.1

--- lib_test/result_query.py
+++ lib_test/result_query.py
@@ -7,11 +7,11 @@ import sys
 import os
 import re
 
-class VacationSet:
+class DataSet:
     def __init__(self, entries):
         self.entries = entries
     def search(self, key, val):
-        return VacationSet([e for e in self.entries if e.properties[key] == val ])
+        return DataSet([e for e in self.entries if e.properties[key] == val ])
     def average(self, key):
         if key == "version":
             print "Error: please input a numeric key"
@@ -27,9 +27,10 @@ class VacationSet:
         for e in self.entries:
             s += e.__str__()
         return s
-class VacationEntry:
+class DataEntry:
     #Constructor
     def __init__(self, version, properties):
+        print properties
         self.properties = { "version" : version}
         self.properties.update(properties)
         self.properties["abort_ratio"] = properties["Aborts"] / float(properties["Starts"])
@@ -39,58 +40,45 @@ class VacationEntry:
         """
             This method is based on the format of entry:
                 --------Following is the entry-------
-            Initializing manager... done.
-            Initializing clients... done.
-                Transactions        = 4194304
-                Clients             = 16
-                Transactions/client = 262144
-                Queries/transaction = 8
-                Relations           = 65536
-                Query percent       = 10
-                Query range         = 6554
-                Percent user        = 80
+            Configurations: 
+                Transactions        = 65536
+                Clients             = 1
+                Transactions/client = 65536
+                Sets/transaction    = 1
+                Relations           = 8
+                Query percent       = 25
+                Query range         = 2
+            Query Range:
+                Client 0   = 1 - 2
             TL2 system ready: GV=GV4
             Running clients... done.
-            Time = 83.392587
-            Checking tables... done.
+            Time = 0.310766
             Deallocating memory... done.
             TL2 system shutdown:
-              GCLOCK=0x7E7020 Starts=16038459 Aborts=11844155
-              Overflows: R=0 W=10608 L=0
+              GCLOCK=0x0 Starts=65536 Aborts=0
+              Overflows: R=0 W=0 L=0
               -------------Entry finishes------------
         """
-        cnt = 0
-        n = 18 
-        skipped = set([1,2,11,12,14,15,16])
         res = []
         version = "boost"
         if "boost" in file_path:
-            print file_path
             version = "boost"
         else:
             version = "tl2"
-
-
         try:
             f = open(file_path)
             properties = {}
+            size = 13
             for line in f:
                 #skip useless lines
-                if cnt+1 in skipped:
-                    pass
-                elif cnt+1 == 17:
-                    valid = line[line.find("Start"):]
-                    properties.update(multiple_equations_to_key_value(valid))
-                elif cnt+1 == 18:
-                    properties.update(multiple_equations_to_key_value(line.split(":")[1]))
-                else:
-                    ret = single_equation_to_key_value(line)
-                    properties.update(ret)
-                cnt += 1
-                cnt %= n
-                if cnt == 0:
-                    res.append(VacationEntry(version, properties))
+                if "Configuration" in line and len(properties) == size:
+                    res.append(DataEntry(version, properties))
                     properties = {}
+                elif "-" in line:
+                    continue
+                elif "=" in line:
+                    values = multiple_equations_to_key_value(line)
+                    properties.update(values)
         except IOError:
             sys.stderr.write("Failed to open file: %s" % (file_path))
         return res
@@ -101,28 +89,46 @@ class VacationEntry:
         return s
 
 def multiple_equations_to_key_value(line):
-    pattern = re.compile("\w+\s*=\w+\s*")
+    #if "Starts" in line:
+        #line = line[line.find("Starts"):]
+    #elif "Overflow" in line:
+        #line = line.split(":")[1]
+
+    print line
+    pattern = re.compile("\w+\s*=\s*\w+\s*")
     l = re.findall(pattern, line)
     res = {}
     for e in l:
-        res.update(single_equation_to_key_value(e))
+        ret = single_equation_to_key_value(e)
+        if ret != None:
+            res.update(ret)
     return res
 
 
     
 def single_equation_to_key_value(line):
     splited = [word.strip() for word in line.split('=')]
-    return { splited[0] : int(float(splited[1])) }
+    ret = None
+    try: 
+        if ("Time" in splited[0]):
+            ret = { splited[0] : float(splited[1]) }
+        else:
+            ret = { splited[0] : int(float(splited[1])) }
+        return  ret
+    except ValueError:
+        #print "unable to convert %s : %s" %(splited[0], splited[1])
+        return None
 def printf(line):
     sys.stdout.write(line.__str__())
 def get_file_list(target_dir):
-    return [ os.path.join(target_dir, file) for file in os.listdir(target_dir) if os.path.isfile(os.path.join(target_dir, file)) if "output" in file] 
+    return [ os.path.join(target_dir, file) for file in os.listdir(target_dir) if os.path.isfile(os.path.join(target_dir, file))] 
     
-    
+
+
 if __name__ == "__main__":
     #argument check
     if len(sys.argv) != 2:
-        sys.stderr.write("Invalid argument. Usage: raw2cvs.py target_dir")
+        sys.stderr.write("Invalid argument. Usage: result_query.py target_dir")
         sys.exit()
     
     target_dir = sys.argv[1]
@@ -131,14 +137,14 @@ if __name__ == "__main__":
     #file list
     entries = []
     for file in get_file_list(target_dir):
-        entries += (VacationEntry.FileToEntries(file))
-    vacation_set = VacationSet(entries)
+        entries += (DataEntry.FileToEntries(file))
+    data_set= DataSet(entries)
     entries1 = entries
-
-    default = vacation_set.search("Transactions", 4194304).search("version", "default")
-    boost = vacation_set.search("Transactions", 4194304).search("version", "boost")
+    default = data_set.search("Transactions", 65536).search("version", "tl2")
+    boost = data_set.search("Transactions", 65536).search("version", "boost")
+    
     i = 1
-    while i <= 32:
+    while i <= 16:
         print "%d clients: " %(i)
         print "\tdefault average time: %.2f" %(default.search("Clients", i)).average("Time")
         print "\tdefault average abort ratio: %.2f" %(default.search("Clients", i)).average("abort_ratio")
