8e60e5cee46a2e3dc5680d1f85ff3886451f0a01
Wenbin Xiao <xwb1989@gmail.com>
2015-01-18 18:30:39 -0500

    citrus basic impl

---
+++ boosted_citrus.cpp
@@ -0,0 +1,87 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  boosted_citrus.cpp
+ *
+ *    Description:  boosted citrus. It uses the same interface as boosted_map.cpp
+ *
+ *        Version:  1.0
+ *        Created:  01/18/2015 04:29:19 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wenbin Xiao(github.com/xwb1989) 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include "boosted_citrus.h"
+#include "urcu.h"
+
+bool_t BoostedCitrus::tm_insert(long key, void* val) {
+    locks.lock(key, AbstractLock::Mode::WRITE);
+    bool result = citrus_insert(citrus_root, key, val);
+    if(result) {
+        LogMap::accessor writer;
+        undo_logs.insert(writer, thread_getId());
+        auto& undo_log = writer->second;
+        undo_log.push_back([this, key]() {
+                return citrus_delete(citrus_root, key);
+                } );
+    }
+    return result;
+}
+
+bool_t BoostedCitrus::tm_remove(long key) {
+    locks.lock(key, AbstractLock::Mode::WRITE);
+    void* val;
+    //get the val associated with the key
+    bool result = citrus_find(citrus_root, key, &val);
+    if(result) {
+        //get the log or create a new one if it does not exist
+        LogMap::accessor writer;
+        undo_logs.insert(writer, thread_getId());
+        auto& undo_log = writer->second;
+
+        assert(citrus_delete(citrus_root, key));
+        undo_log.push_back([this, key, val]() {
+                return citrus_insert(citrus_root, key, val);            
+                });
+    }
+    return result;
+}
+
+bool_t BoostedCitrus::tm_contains(long key) {
+    locks.lock(key, AbstractLock::Mode::READ);
+    return citrus_contains(citrus_root, key);
+}
+
+void* BoostedCitrus::tm_find(long key) {
+    locks.lock(key, AbstractLock::Mode::READ);
+    void* val;
+    if (citrus_find(citrus_root, key, &val)) {
+        return val;
+    }
+    return nullptr;
+}
+
+bool_t BoostedCitrus::insert(long key, void* val) {
+    bool result = citrus_insert(citrus_root, key, val);
+    return result;
+}
+
+bool_t BoostedCitrus::remove(long key) {
+    return citrus_delete(citrus_root, key);
+}
+
+bool_t BoostedCitrus::contains(long key) {
+    return citrus_contains(citrus_root, key);
+}
+
+void* BoostedCitrus::find(long key) {
+    void* val;
+    if (citrus_find(citrus_root, key, &val)) {
+        return val;
+    }
+    return nullptr;
+}
---
+++ boosted_citrus.h
@@ -0,0 +1,50 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  boosted_citrus.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/18/2015 04:40:04 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wenbin Xiao (github.com/xwb1989) 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef BOOSTED_CITRUS_H
+#define BOOSTED_CITRUS_H
+
+#include "base_container.h"
+#include "citrus.h"
+
+class BoostedCitrus: public BaseContainer {
+    private:
+        citrus_node citrus_root;
+
+    public:
+        //Constructor
+        BoostedCitrus();
+
+        //Destructor
+        ~BoostedCitrus();
+
+        //tx operations
+        bool_t tm_insert(long key, void* val);
+        bool_t tm_remove(long key);
+        bool_t tm_contains(long key);
+        void* tm_find(long key);
+
+        //standard operations
+        bool_t insert(long key, void* val);
+        bool_t remove(long key);
+        bool_t contains(long key);
+        void* find(long key);
+
+};
+
+#endif
