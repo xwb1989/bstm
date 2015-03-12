ca77354762438c36788de61516f566b67180ca57
Wenbin Xiao <xwb1989@gmail.com>
2015-03-12 19:09:45 -0400

    include

--- include/bench.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef BENCH_TREE
-#define BENCH_TREE
-
-/*!
- * Bench the different structures
- */
-void bench();
-
-#endif
--- include/test.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_TREES
-#define TEST_TREES
-
-/*!
- * Launch all the tests. 
- */
-void test();
-
-#endif
