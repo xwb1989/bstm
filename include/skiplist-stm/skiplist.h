fb01fe22c03b2755f0150b6ccd3ec0d8060cc738
Wenbin Xiao <xwb1989@gmail.com>
2015-03-19 22:07:25 -0400

    add MAP interface

--- include/skiplist-stm/skiplist.h
+++ include/skiplist-stm/skiplist.h
@@ -1,91 +1,45 @@
 /*
- * File:
- *   skiplist.h
- * Author(s):
- *   Vincent Gramoli <vincent.gramoli@epfl.ch>
- * Description:
- *   Stress test of the skip list implementation.
+ * =====================================================================================
  *
- * Copyright (c) 2009-2010.
+ *       Filename:  skiplist-map.h
  *
- * skiplist.h is part of Synchrobench
- * 
- * Synchrobench is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, version 2
- * of the License.
+ *    Description:  MAP interface for skiplist
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *        Version:  1.0
+ *        Created:  03/19/2015 10:02:04 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Wenbin Xiao
+ *   Organization:  
+ *
+ * =====================================================================================
  */
 
-#include <assert.h>
-#include <getopt.h>
-#include <limits.h>
-#include <pthread.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/time.h>
-#include <time.h>
-#include <stdint.h>
+#ifndef SKIPLIST_H
+#define SKIPLIST_H
 
 
-#include "tm.h"
+#  include "sl-map.h"
 
-#define DEFAULT_DURATION                10000
-#define DEFAULT_INITIAL                 256
-#define DEFAULT_NB_THREADS              1
-#define DEFAULT_RANGE                   0x7FFFFFFF
-#define DEFAULT_SEED                    0
-#define DEFAULT_UPDATE                  20
-#define DEFAULT_ELASTICITY              4
-#define DEFAULT_ALTERNATE               0
-#define DEFAULT_EFFECTIVE               1
+#  define MAP_T                       sl_map_t
+#  define MAP_ALLOC(hash, cmp)        sl_map_new()
+#  define MAP_FREE(map)               sl_delete(map)
+#  define MAP_CONTAINS(map, key)      sl_contains(map, key)
+#  define MAP_FIND(map, key)          sl_find(key)
+#  define MAP_INSERT(map, key, data)  sl_insert(map, key, (void*) data)
+#  define MAP_REMOVE(map, key)        sl_remove(map, key)
 
-#define XSTR(s)                         STR(s)
-#define STR(s)                          #s
 
-#define ATOMIC_CAS_MB(a, e, v)          (AO_compare_and_swap_full((volatile AO_t *)(a), (AO_t)(e), (AO_t)(v)))
-#define ATOMIC_FETCH_AND_INC_FULL(a)    (AO_fetch_and_add1_full((volatile AO_t *)(a)))
+//TM operations
 
-extern unsigned int global_seed;
-#ifdef TLS
-extern __thread unsigned int *rng_seed;
-#else /* ! TLS */
-extern pthread_key_t rng_seed_key;
-#endif /* ! TLS */
-extern unsigned int levelmax;
+#  define TMMAP_CONTAINS(map, key)    \
+    TM_sl_contains(TM_ARG map, key)
+#  define TMMAP_FIND(map, key)      \
+    TM_sl_find(TM_ARG, map, key)
+#  define TMMAP_INSERT(map, key, data) \
+    TM_sl_insert(TM_ARG map, key, (void*)data)
+#  define TMMAP_REMOVE(map, key)  \
+    TM_sl_remove(TM_ARG map, key)
 
-#define TRANSACTIONAL                   d->unit_tx
-
-typedef intptr_t val_t;
-//typedef intptr_t level_t;
-#define VAL_MIN                         INT_MIN
-#define VAL_MAX                         INT_MAX
-
-typedef struct sl_node {
-  val_t val;
-  intptr_t deleted;
-  int toplevel;
-  struct sl_node *next[1];
-} sl_node_t;
-
-typedef struct sl_intset {
-  sl_node_t *head;
-} sl_intset_t;
-
-int get_rand_level();
-int floor_log_2(unsigned int n);
-
-sl_node_t *sl_new_simple_node(val_t val, int toplevel, int transactional);
-sl_node_t *sl_new_node(val_t val, sl_node_t *next, int toplevel, int transactional);
-void sl_delete_node(sl_node_t *n);
-
-sl_intset_t *sl_set_new();
-void sl_set_delete(sl_intset_t *set);
-unsigned long sl_set_size(sl_intset_t *set);
-
-inline long rand_range(long r); /* declared in test.c */
+#endif
